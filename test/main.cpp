b79a40e9da8a45e2c9529eef3cd47090f0723474
chengheai <842385139@qq.com>
2019-05-22 23:00:28 +0800

    hello world

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by 程和爱 on 2019/5/22.
+//  Copyright © 2019 程和爱. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
